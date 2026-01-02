be919b7ddb1a2e64f626fa02d17384c3a28a7b65
AnitaPK <anita.khamitkar@gmail.com>
2026-01-02 12:02:01 +0530

    up to prfile image

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getLoggedUser } from '../api/userAPI'
 
 const Profile = () => {
+  const [ user,setUSer] = useState()
+  async function fetchData(){
+      const res = await getLoggedUser()
+      if(res.data.success){
+        setUSer(res.data.user)
+      }
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div>
-      Profile
-    </div>
+    <>
+      <h1>Profile</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className='p-4'>
+                <h3>{user?.name}</h3>
+                <p>{user?.email}</p>
+            </div>
+          </div>
+          <div className="col">
+            <img src={`${user?.imagePath}`} alt={user?.name} />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
